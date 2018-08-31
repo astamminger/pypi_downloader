bf5eb8c0248dd4cfc7eb66d0e8b60fc3f43bad07
A. Stamminger <stammingera@gmail.com>
2018-08-31 12:18:23 +0200

    Added function to create directories

--- pypi_crawler/pypi_crawler.py
+++ pypi_crawler/pypi_crawler.py
@@ -4,12 +4,23 @@ import requests
 
 _GENERIC_ADDRESS = "https://pypi.org/pypi/{package}/json"
 
-def build_package_cache(settings, package_list):
-    """Download all package version for specified package list"""
+def create_directory(dirname):
+    """Check if a directory exists and create it if it doesn't."""
+
+    try:
+        os.makedirs(dirname)
+    except OSError as e: 
+        if e.errno != errno.EEXIST:
+            raise
+    
+    return None
+
+def build_package_cache(settings, package):
+    """Download all package versions from PyPI for specified package."""
 
     for package in package_list:
         package_pypi_address = _GENERIC_ADDRESS.format(package=package)
         package_info = requests.get(package_pypi_address).json()
 
         print(package_info)
-        
+
