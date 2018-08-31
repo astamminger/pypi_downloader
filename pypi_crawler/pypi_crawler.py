b525cd718d711acb8b64eb9548d38ccec52fc0f8
A. Stamminger <stammingera@gmail.com>
2018-08-31 12:30:02 +0200

    Implemented function to resolve package urls

--- pypi_crawler/pypi_crawler.py
+++ pypi_crawler/pypi_crawler.py
@@ -1,8 +1,44 @@
 #!/usr/bin/env python
 
+
+import os
+import errno
 import requests
 
+from pgg_resources import Requirement
+
+
 _GENERIC_ADDRESS = "https://pypi.org/pypi/{package}/json"
+
+
+def build_package_cache(settings, package):
+    """Download all package versions from PyPI for specified package."""
+
+    
+
+
+
+def resolve_url_list(package):
+    """Build a url list for packages matching the specifications."""
+
+    requires = Requirement(package)
+
+    package_addr = _GENERIC_ADDRESS.format(package=requires.name)
+    package_data = requests.get(package_addr).json()
+    
+    all_versions = package_data["releases"].keys()
+    wanted_versions = [v for v in all_versions if (requires.__contains__(v))]
+
+    url_list = []
+    for version in wanted_versions:
+        package_info = package_data['releases'].get(version)
+        filename = str(package_info.get('filename')) # file name
+        url = str(package_info.get('url'))           # url
+        project_name = str(filename.split('-')[0])   # sub-folder in cache
+        url_list.append((project_name, filename, url))
+
+    return url_list
+
 
 def create_directory(dirname):
     """Check if a directory exists and create it if it doesn't."""
@@ -14,13 +50,4 @@ def create_directory(dirname):
             raise
     
     return None
-
-def build_package_cache(settings, package):
-    """Download all package versions from PyPI for specified package."""
-
-    for package in package_list:
-        package_pypi_address = _GENERIC_ADDRESS.format(package=package)
-        package_info = requests.get(package_pypi_address).json()
-
-        print(package_info)
 
