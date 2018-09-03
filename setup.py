740eb4d5f4d5f3876f00292603cb7097b57d29a1
A. Stamminger <stammingera@gmail.com>
2018-09-04 00:04:03 +0200

    Added URL to setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@
 
 from setuptools import setup
 
-with open("README", 'r') as f:
+with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(
-   name='pypi_crawler',
-   version='0.1',
+   name='pypi_downloader',
+   version='1.0',
    description='A small module to download packages from PyPi',
    license="MIT",
    long_description=long_description,
    author='Andreas Stamminger',
    author_email='andreas.stamminger@pm.me',
    scripts=['bin/sync_package_cache'],
-#   url="http://www.foopackage.com/",
-   packages=['pypi_crawler'],
+   url="https://github.com/astamminger/pypi_downloader",
+   packages=['pypi_downloader'],
    install_requires=['requests', 'configparser'], 
 )
