42a7fd74b2279cc015bc14cb1e5335bdde7bbf6f
A. Stamminger <stammingera@gmail.com>
2018-09-04 23:07:30 +0200

    Made package python2.7 compatible

--- pypi_downloader/__init__.py
+++ pypi_downloader/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import absolute_import
+
 from pypi_downloader.pypi_downloader import (build_package_cache, 
                                              create_directory)
